78704692adfbf3946e0864f2f3e6a9d37efb4e11
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 21:53:32 +0100

    Added Sudoku services.

---
+++ Sudoku_Application/Services/SudokuService.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku_Application.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Application.Services
+{
+    public class SudokuService: ISudokuService
+    {
+    }
+}
