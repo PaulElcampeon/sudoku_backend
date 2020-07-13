ebdf3260735a77898094e64901a1d4dc8dccd945
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 21:54:24 +0100

    Added SudokuSolution model.

---
+++ Sudoku_Application/Models/SudokuSolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Application.Models
+{
+    public class SudokuSolution
+    {
+        public int[,] solution { get; set; }
+    }
+}
