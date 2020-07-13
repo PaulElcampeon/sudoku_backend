7cc88c9074f8399216740f35e19a5ffa7fc5055c
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 21:35:00 +0100

    Added static files.

---
+++ Sudoku_Application/wwwroot/precache-manifest.0a806944ef03370a16d59edc32250cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e5c3871e59cc987e72f6de26c61641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6552f62345e83144074e",
+    "url": "/static/css/main.04721634.chunk.css"
+  },
+  {
+    "revision": "e69ac6c41bba9f4e393b",
+    "url": "/static/js/2.6139bd82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6139bd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6552f62345e83144074e",
+    "url": "/static/js/main.31252310.chunk.js"
+  },
+  {
+    "revision": "e7f52685bb66d906327a",
+    "url": "/static/js/runtime-main.a770f99f.js"
+  }
+]);
