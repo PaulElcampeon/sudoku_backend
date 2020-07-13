42265c17d09cfb419e7b19a8e4232d32875caaf4
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 21:53:51 +0100

    Register services on start up.

--- Sudoku_Application/Startup.cs
+++ Sudoku_Application/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Sudoku_Application.Service;
 
 namespace Sudoku_Application
 {
@@ -26,6 +27,7 @@ namespace Sudoku_Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<ISudokuService, Services.SudokuService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +36,12 @@ namespace Sudoku_Application
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            } else
+            {
+                app.UseHsts();
             }
+
+            app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
