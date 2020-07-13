88836a5bd02003adeb78a5b21db45099319bad93
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 21:54:58 +0100

    Added api controller for Sudoku.

---
+++ Sudoku_Application/Controllers/SudokuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sudoku_Application.Models;
+
+namespace Sudoku_Application.Controllers
+{
+    [Route("api/sudoku")]
+    [ApiController]
+    public class SudokuController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<SudokuSolution> GetSolution()
+        {
+
+        }
+    }
+}
