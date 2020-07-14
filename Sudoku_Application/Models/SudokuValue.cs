413d3f6f549572bc7131903a72ffb6a71eb21a58
PaulElcampeon <pauloladele@live.co.uk>
2020-07-14 12:03:05 +0100

    Added new model.

---
+++ Sudoku_Application/Models/SudokuValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Application.Models
+{
+    public class SudokuValue
+    {
+        public int value { get; set; }
+        public bool wasGiven { get; set; }
+    }
+}
