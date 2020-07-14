ca50a231c466230d4adc3e697aeb4a851c45d3ff
PaulElcampeon <pauloladele@live.co.uk>
2020-07-14 14:54:10 +0100

    Added new model.

---
+++ Sudoku_Application/Models/SudokuAnswerRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sudoku_Application.Models
+{
+    public class SudokuAnswerRequest
+    {
+        public SudokuValue[,] originalBoard { get; set; }
+
+        public SudokuValue[,] edittedBoard { get; set; }
+
+    }
+}
