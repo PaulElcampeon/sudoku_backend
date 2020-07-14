516b7bd1e7c38f717f0f67dd1b2b1a6d7e14e28c
PaulElcampeon <pauloladele@live.co.uk>
2020-07-14 08:42:56 +0100

    Added service and implemented functionality for GetSolution action.

--- Sudoku_Application/Controllers/SudokuController.cs
+++ Sudoku_Application/Controllers/SudokuController.cs
@@ -5,17 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sudoku_Application.Models;
+using Sudoku_Application.Service;
 
 namespace Sudoku_Application.Controllers
 {
-    [Route("api/sudoku")]
+    [Route("api/sudoku/")]
     [ApiController]
     public class SudokuController : ControllerBase
     {
-        [HttpPost]
-        public ActionResult<SudokuSolution> GetSolution()
-        {
+        private readonly ISudokuService _service;
 
+        public SudokuController(ISudokuService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("solution")]
+        public ActionResult<SudokuSolution> GetSolution(SudokuSolutionRequest solutionRequest)
+        {
+            if (_service.IsRequestValid(solutionRequest))
+            {
+                SudokuSolution solution = _service.FindSolution(solutionRequest);
+
+                return solution;
+            }
+
+            return BadRequest();
         }
     }
 }
